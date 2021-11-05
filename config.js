d18de993c4b98512746c6450e652379ca810ff12
lesiukv <lesiyk47@gmail.com>
2021-11-05 13:07:16 +0200

    df';lkl

--- config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  secretKey: "12345-67890-09876-54321",
-  mongoUrl: "mongodb://localhost:27017/conFusion",
-  facebook: {
-    clientId: '635980747406078',
-    clientSecret: '812b83f6b30f7e640434353c4c72a8aa'
-  }
-};
